00240ccb3dcae2632bea6aeea91a55d741189dff
Louis Chatriot <louis.chatriot@gmail.com>
2020-06-26 19:28:08 +0200

    Breaking out function arguments

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 console.log("===");
 
 
+function plus(a, b) { return a + b }
+function invert(a) { return - a }
+
+var functions = {
+    "invert": invert,
+    "plus": plus
+}
+
+
 function Node(value, cell, func, left, right) {
     this.value = value
     this.cell = cell
@@ -35,7 +44,7 @@ function extract_cell_name(f) {
 
 
 
-var single_character_elements = ["(", ")", "+", "*", "-", "/"]
+var single_character_elements = ["(", ")", "+", "*", "-", "/", ","]
 
 // Convention: functions are lowercase, cell coordinates upper case
 function parse_formula(f) {
@@ -43,12 +52,12 @@ function parse_formula(f) {
     if (f[0] === "'") { return new Node(f.substring(1)) }
     if (f[0] !== "=") { return new Node(f) }
 
-    var elements = [], func_name, cell_name, number, length
-
     // We have a formula
+    var elements = [], func_name, cell_name, number, length
     var i = 1
     f = f.replace(/ /g, "")
 
+    // Extract elements
     while (i < f.length) {
         if (single_character_elements.indexOf(f[i]) !== -1) {
             elements.push(f[i])
@@ -89,9 +98,89 @@ function parse_formula(f) {
         }
     }
 
+    // Create calculation tree
+
+    console.log(elements);
+
+    tree = create_tree(elements)
+
+    console.log(tree);
+
+
 
     return elements
 }
+
+
+
+
+function create_tree(elements) {
+    if (elements.length === 0) { return null }
+
+    var elt = elements[0]
+
+    if (elements.length === 1) {
+        if (elt[0] >= 'A' && elt[0] <= 'Z') {
+            return new Node(null, elt, null, null, null)
+        } else if (! isNaN(elt)) {
+            return new Node(elt, null, null, null, null)
+        } else {
+            throw new Error("Unexpected element type")
+        }
+    }
+
+    if (elt[0] >= 'a' && elt[0] <= 'z') {   // Function
+        if (!functions[elt]) {
+            throw new Error("Unknown function: " + elt)
+        }
+
+        if (elements.length === 1) {
+            throw new Error("Nothing passed to function " + elt)
+        }
+
+        var func = functions[elt]
+        var args = [], opened = 1, i = 2, arg
+
+        while (opened > 0 && i < elements.length) {
+            arg = []
+            while (i < elements.length) {
+                if (opened === 1 && (elements[i] === "," || elements[i] === ")")) { break }
+
+                if (elements[i] === "(") {
+                    opened += 1
+                }
+
+                if (elements[i] === ")") {
+                    opened -= 1
+                }
+
+                arg.push(elements[i])
+                i += 1
+            }
+
+            args.push(arg)
+
+            if (elements[i] === ")") {
+                break
+            }
+
+            i += 1
+        }
+
+        console.log(args);
+
+        throw new Error("Missing closing parenthesis")
+
+    }
+
+
+}
+
+
+
+
+
+
 
 
 
